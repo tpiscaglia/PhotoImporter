1829d9311c8dcd6a328b0decb446f59954614186
tpiscaglia <61087490+tpiscaglia1@users.noreply.github.com>
2020-02-15 09:07:21 -0600

    Initial commit
    Add project to source control
    
    Co-Authored-By: Tyler Piscaglia <tpiscaglia@users.noreply.github.com>

---
+++ PictureImporter/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PictureImporter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIconContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.notifyIconContextMenuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "Click here to import photos from USB";
+            this.notifyIcon.BalloonTipTitle = "Import photos";
+            this.notifyIcon.ContextMenuStrip = this.notifyIconContextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Import photos from USB";
+            // 
+            // notifyIconContextMenuStrip
+            // 
+            this.notifyIconContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.notifyIconContextMenuStrip.Name = "notifyIconContextMenuStrip";
+            this.notifyIconContextMenuStrip.Size = new System.Drawing.Size(104, 48);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 136);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Importing picture 0 of 0...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 50);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(435, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 161);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainWindow";
+            this.Text = "Photo Importer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.notifyIconContextMenuStrip.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip notifyIconContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PictureImporter/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Threading;
+
+namespace PictureImporter
+{
+    public partial class MainWindow : Form
+    {
+        private string _copyToPath;
+        private string _copyabelText = "Importing picture {0} of {1}...";
+        private delegate void SafeCallDelegate();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            //foreach (var property in instance.Properties)
+            //{
+            //    Console.WriteLine(property.Name + " = " + property.Value);
+            //}
+            string driveLetter = instance.Properties["Name"].Value.ToString() + "\\";
+
+            List<string> pictures = GetImages(driveLetter);
+
+            if (pictures.Count > 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("Pictures detected!  Would you like to import them?", "Import Photos?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    RestoreWindow();
+                    _copyToPath = Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Pictures\\" + DateTime.Now.ToString("MM-dd-yyyy")).FullName;
+
+                    CopyFiles(pictures, _copyToPath);
+                    DeleteFiles(pictures);
+                }
+                else
+                    MessageBox.Show("OK, pictures were not imported.", "Import Canceled", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                HideWindow();
+            }
+        }
+
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " - " + property.Value);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_LogicalDisk'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_LogicalDisk'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+        }
+
+        private List<string> GetImages(string directory)
+        {
+            List<string> extensions = new List<string> { ".jpg", ".jpeg", ".png", ".gif", ".tif", ".bmp" };
+
+            return Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(x => extensions.IndexOf(Path.GetExtension(x)) >= 0).ToList();
+        }
+
+        private void CopyFiles(List<string> files, string destinationDirectory)
+        {
+            if (files.Count > 0)
+            {
+                DisplayProgress(files.Count, 1);
+                foreach (var file in files)
+                {
+                    UpdateProgressBar();
+                    UpdateProgressBarText(files.IndexOf(file) + 1, files.Count);
+                    File.Copy(file, $"{destinationDirectory}{file.Remove(0, file.LastIndexOf('\\'))}");
+                    // For Testing
+                    Thread.Sleep(1000);
+                }
+                HideWindow();
+                HideProgress();
+                MessageBox.Show("All photos imported.  You can find them in your pictures folder under today's date.", "Photos Imported!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void DeleteFiles(List<string> files)
+        {
+            foreach (var filePath in files)
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        private void DisplayProgress(int max, int step)
+        {
+            if (progressBar1.InvokeRequired)
+                Invoke(new SafeCallDelegate(() => { progressBar1.Visible = true; progressBar1.Maximum = max; progressBar1.Step = step; }));
+            else
+            {
+                progressBar1.Maximum = max;
+                progressBar1.Step = step;
+                progressBar1.Visible = true;
+            }
+            if (label1.InvokeRequired)
+                Invoke(new SafeCallDelegate(() => { UpdateProgressBarText(1, max); label1.Visible = true; }));
+            else
+            {
+                UpdateProgressBarText(1, max);
+                label1.Visible = true;
+            }
+        }
+
+        private void UpdateProgressBar()
+        {
+            if (progressBar1.InvokeRequired)
+                Invoke(new SafeCallDelegate(() => progressBar1.PerformStep()));
+            else
+                progressBar1.PerformStep();
+        }
+
+        private void UpdateProgressBarText(int current, int max)
+        {
+            Invoke(new SafeCallDelegate(() => label1.Text = string.Format(_copyabelText, current, max)));
+        }
+
+        private void HideProgress()
+        {
+            Invoke(new SafeCallDelegate(() => { progressBar1.Visible = false; label1.Visible = false; }));
+        }
+
+        private void RestoreWindow()
+        {
+            Invoke(new SafeCallDelegate(() => { CenterToScreen(); Show(); BringToFront(); TopMost = true; Focus(); WindowState = FormWindowState.Normal; notifyIcon.Visible = false; }));
+        }
+
+        private void HideWindow()
+        {
+            Invoke(new SafeCallDelegate(() => { Hide(); notifyIcon.Visible = true; }));
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                HideWindow();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            HideWindow();
+        }
+    }
+}
